90bdba8d6c22d8c1a4102bff93a07f6eb80df59d
N0153128 <noisebro@hotmail.com>
2020-07-14 18:46:12 +0100

    added extra functionality

--- get_and_store.py
+++ get_and_store.py
@@ -3,7 +3,8 @@
 from bs4 import BeautifulSoup
 import requests
 from peewee import *
-from unidecode import unidecode
+from random import randint
+from time import sleep
 
 db = SqliteDatabase('storage.db', autoconnect=True)
 
@@ -14,6 +15,9 @@ class Quotes(Model):
 
     class Meta:
         database = db
+        indexes = (
+            (('quote', 'author'), True),
+        )
 
 
 db.create_tables([Quotes])
@@ -30,13 +34,17 @@ class GetAndStoreAndMore:
         page = requests.get(url)
         soup = BeautifulSoup(page.content, 'html.parser')
         quote = soup.find('h3', class_='blockquote-text')
-        q = quote.a['title'][17:].split('“ —')[0].replace('\\xa0', '').strip('„')
-        a = quote.a['title'][17:].split('“ —')[1].replace('\\xa0', '').strip('„')
+        q = quote.a['title'][17:].split('“ —')[0].strip('„')
+        a = quote.a['title'][17:].split('“ —')[1].strip('„')
         return q, a
 
     def add_quote(self, quote_):
-        query = Quotes(quote=quote_[0], author=quote_[1])
-        query.save()
+        try:
+            query = Quotes(quote=quote_[0], author=quote_[1])
+            query.save()
+        except Exception as e:
+            print(e)
+            pass
 
     def list_quotes(self):
         all = []
@@ -50,5 +58,24 @@ class GetAndStoreAndMore:
             query = i.delete()
             query.execute()
 
+    def get_random_quote(self):
+        try:
+            return self.list_quotes()[randint(1, len(self.list_quotes())-1)][1]
+        except IndexError:
+            self.get_random_quote()
+
+    def add_lots(self, lots):
+        for i in range(lots):
+            self.add_quote(self.get())
+            sleep(1)
+
+
+    def add_particular(self, q, a):
+        try:
+            query = Quotes(quote=q, author=a)
+            query.save()
+        except Exception as e:
+            print(e)
+            pass
 
 obj = GetAndStoreAndMore()
